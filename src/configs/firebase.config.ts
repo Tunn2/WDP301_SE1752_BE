c0846a3b3f04cf77e82214845de68c8c0b179b21
tungnk <tungnk1404@gmail.com>
2025-05-22 00:55:03 +0700

    fix: firebase config

--- src/configs/firebase.config.ts
+++ src/configs/firebase.config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-require-imports */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { initializeApp } from 'firebase-admin/app';
@@ -6,11 +7,11 @@ import { getFirestore } from 'firebase-admin/firestore';
 import * as admin from 'firebase-admin';
 
 // Load service account từ file JSON
-const serviceAccount = require('../../serviceAccount.json');
+import serviceAccount from '../../serviceAccountKey.json';
 
 // Khởi tạo Firebase Admin
 const firebaseApp = initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccount as any),
   storageBucket: 'netflix-clone-618f2.appspot.com', // Lấy từ project_id hoặc cấu hình Firebase Storage
 });
 
