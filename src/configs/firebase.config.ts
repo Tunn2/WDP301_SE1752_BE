c105f51a1c3b6bb613641d20f6a386ae77e6e695
tungnk <tungnk1404@gmail.com>
2025-05-22 21:44:44 +0700

    fix: file key

--- src/configs/firebase.config.ts
+++ src/configs/firebase.config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unnecessary-type-assertion */
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-require-imports */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
@@ -5,7 +6,8 @@ import { initializeApp } from 'firebase-admin/app';
 import { getStorage } from 'firebase-admin/storage';
 import { getFirestore } from 'firebase-admin/firestore';
 import * as admin from 'firebase-admin';
-import serviceAccount from '../../serviceAccountKey.json';
+// import serviceAccount from '../../serviceAccountKey.json';
+const serviceAccount = JSON.parse(process.env.FIREBASE_KEY_JSON as string);
 
 const firebaseApp = initializeApp({
   credential: admin.credential.cert(serviceAccount as any),
