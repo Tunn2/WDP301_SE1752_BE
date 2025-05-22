4ce8216c792c44c6b6d9106f7b10ffa214871fea
tungnk <tungnk1404@gmail.com>
2025-05-22 22:30:44 +0700

    fix 3

--- src/configs/firebase.config.ts
+++ src/configs/firebase.config.ts
@@ -6,8 +6,8 @@ import { initializeApp } from 'firebase-admin/app';
 import { getStorage } from 'firebase-admin/storage';
 import { getFirestore } from 'firebase-admin/firestore';
 import * as admin from 'firebase-admin';
-// import serviceAccount from '../../serviceAccountKey.json';
-const serviceAccount = JSON.parse(process.env.FIREBASE_KEY_JSON as string);
+import serviceAccount from '../../serviceAccountKey.json';
+// const serviceAccount = JSON.parse(process.env.FIREBASE_KEY_JSON as string);
 
 const firebaseApp = initializeApp({
   credential: admin.credential.cert(serviceAccount as any),
