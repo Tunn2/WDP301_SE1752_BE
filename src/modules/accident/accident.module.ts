fa5fb7500499f210edeed4a09f34cfcd9c20184a
tungnk <tungnk1404@gmail.com>
2025-07-26 21:03:48 +0700

    siuu

--- src/modules/accident/accident.module.ts
+++ src/modules/accident/accident.module.ts
@@ -4,9 +4,10 @@ import { AccidentController } from './accident.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Accident } from './entities/accident.entity';
 import { Student } from 'src/modules/student/entities/student.entity';
+import { ParentStudent } from '../user/entities/parent-student.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Accident, Student])],
+  imports: [TypeOrmModule.forFeature([Accident, Student, ParentStudent])],
   controllers: [AccidentController],
   providers: [AccidentService],
 })
